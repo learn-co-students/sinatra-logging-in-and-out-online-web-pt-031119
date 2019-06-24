8a408ebd24745a8e8a830b0a4dd0a00e0ab47504
MKanko <mkanko66@gmail.com>
2019-06-24 09:35:44 -0700

    completed sinatra-logging-in-and-out-lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,11 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(params[:username]) 
-
-    if @user
+    @user = User.find_by(username: params[:username])
+    #binding.pry  
+    if @user 
       session[:user_id] = @user.id
+      #binding.pry 
       redirect to '/account'
     else
       erb :error
@@ -22,15 +23,18 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    @user = User.find_by(session[:user_id])
-    erb :account
+    @user = User.find_by_id(session[:user_id])
+    if @user 
+      erb :account
+    else
+      erb :error
+    end 
   end
 
   get '/logout' do
-    session.clear
-    redirect to "/"   
+    session.destroy 
+    redirect to '/'     
   end
-
 
 end
 
