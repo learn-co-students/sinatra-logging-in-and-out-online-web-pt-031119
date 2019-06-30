579aa0b0dfcd4809ebe5cb679bc6b82580213116
AJ Guiles <ashiguiles@gmail.com>
2019-06-30 01:20:51 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
+class User < ActiveRecord::Base
+  # makes sure no one can sign up without inputting their name, email, and password. 
+  validates_presence_of :name, :password, :balance 
 
+
+
+end
 
