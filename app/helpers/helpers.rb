8e39d977eb5f68b81ffe18a832b9756a0761f318
Samuel T <ssamt1142@gmail.com>
2019-07-07 21:40:55 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,15 @@
+
 class Helpers
+  def self.current_user(session)
+    @user = User.find_by_id(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    !!session[:user_id] # integer or nil
+
+    # # !! Double bang will ask the question is_it_truthy?
+    # is_it_truthy?(nil) # True
+    # !!2 # True
+    # !!nil # False
+  end
 end
