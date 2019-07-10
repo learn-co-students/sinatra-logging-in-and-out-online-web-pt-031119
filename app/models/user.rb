8b0f19b8685fd6e6f80554bbc6384762c40077e7
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-07-10 21:38:59 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
+class User
+  attr_accessor :username, :password, :balance
 
+    def initialize(username, password, balance)
+      @username = username
+      @password = password
+      @balance = balance
+    end
 
+    def save
+      self.class.all << self
+    end
+
+    def self.create
+      user = self.new
+      user.save
+      user
+    end
+end
